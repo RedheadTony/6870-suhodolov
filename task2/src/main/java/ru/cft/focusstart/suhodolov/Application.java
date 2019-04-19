e054836813a0126c92cae23c09c92c33f7bf595c
mrurec <yura.suhodolov@gmail.com>
2019-04-19 16:06:03 +0700

    Task 2 - completed.

---
+++ task2/src/main/java/ru/cft/focusstart/suhodolov/Application.java
@@ -0,0 +1,99 @@
+package ru.cft.focusstart.suhodolov;
+
+import ru.cft.focusstart.suhodolov.shapes.*;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Application {
+
+    private static BufferedReader reader;
+    private static FileWriter writer;
+    private static Shape shape;
+
+    public static void main(String[] args) {
+        parseArgs(args);
+        if (reader != null) {
+            readInputFile();
+        }
+        if (shape != null) {
+            writeInfo();
+        }
+    }
+
+    /**
+     * Метод, который парсит аргументы командной строки
+     *
+     * @param args строковый массив, содержащий аргументы
+     */
+    private static void parseArgs(String[] args) {
+        try {
+            reader = new BufferedReader(new FileReader(args[0]));
+            if (args.length >= 2) {
+                writer = new FileWriter(args[1], false);
+            }
+        } catch (IOException ex) {
+            System.out.println("Problem with a input/output files");
+        }
+    }
+
+    /**
+     * Метод, который парсит параметры фигуры из строки
+     *
+     * @param strParams строка, содержащая параметры
+     * @return ArrayList, содержащий параметры в формате Double
+     * @throws NumberFormatException выбрасывается, когда в строке встречаются не числовые параметры
+     */
+    private static ArrayList<Double> parseShapeParams(String strParams) throws NumberFormatException {
+        ArrayList<Double> params = new ArrayList<>();
+        String[] strParamsArr = strParams.split("[ ]");
+
+        for (String s : strParamsArr) {
+            params.add(Double.parseDouble(s));
+        }
+
+        return params;
+    }
+
+    /**
+     * Метод, который считывает информацию из входного файла и инициализирует объект фигуры с полученными параметрами
+     */
+    private static void readInputFile() {
+        try {
+            String shapeType = reader.readLine();
+            String strParams = reader.readLine();
+            ArrayList<Double> params = parseShapeParams(strParams);
+
+            if (shapeType.equals(ShapeType.CIRCLE.name()) && params.size() == 1) {
+                shape = new Circle(params.get(0));
+            } else if (shapeType.equals(ShapeType.RECTANGLE.name()) && params.size() == 2) {
+                shape = new Rectangle(params.get(0), params.get(1));
+            } else if (shapeType.equals(ShapeType.TRIANGLE.name()) && params.size() == 3) {
+                shape = new Triangle(params.get(0), params.get(1), params.get(2));
+            } else {
+                System.out.println("Invalid information in input file");
+            }
+            reader.close();
+        } catch (IOException ex) {
+            System.out.println("Problem with reading information from input file");
+        } catch (NumberFormatException ex) {
+            System.out.println("Invalid parameters format");
+        }
+    }
+
+    /**
+     * Метод, который записывает информацию об объекте либо в выходной файл, либо в консоль
+     */
+    private static void writeInfo() {
+        try {
+            if (writer != null) {
+                writer.write(shape.getInformation().toString());
+                writer.close();
+            } else {
+                System.out.println(shape.getInformation());
+            }
+        } catch (IOException ex) {
+            System.out.println("Problem with writing information to output file");
+        }
+    }
+}
